6169bbd99ced9fd79a17e010a5b63e81fc4d15e9
Daphne <121971548+daphnewan@users.noreply.github.com>
2023-02-17 21:05:44 -0800

    3.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,8 +28,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
-
-        if(collision.gameObject.CompareTag("Pbstacle"))
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+        }
     }
 }
